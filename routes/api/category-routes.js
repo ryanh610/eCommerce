9d6ba6c022b0489e70ea06f24e8a2761153137ff
Ryan Howley <rjh623@verizon.net>
2022-04-22 11:39:47 -0400

    set up category routes

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -1,27 +1,47 @@
 const router = require('express').Router();
 const { Category, Product } = require('../../models');
-// The `/api/categories` endpoint
 
 router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+  Category.findAll({include: [Product]})
+  .then((data) => res.json(data))
+  .catch((err) => console.error('Error: ', err));
 });
 
 router.get('/:id', (req, res) => {
-  // find one category by its `id` value
-  // be sure to include its associated Products
+  Category.findOne({where: {id: req.params.id}, include: {model: Product, attributes:['category_id']}})
+  .then((data) => res.json(data))
+  .catch((err) => console.error('Error: ', err));
 });
 
 router.post('/', (req, res) => {
-  // create a new category
+  Category.create({category_name: req.params.category_name})
+  .then((data) => res.json(data))
+  .catch((err) => console.error('Error: ', err));
 });
 
 router.put('/:id', (req, res) => {
-  // update a category by its `id` value
+  Category.update({category_name: req.body.category_name}, {where: {id: req.params.id}})
+  .then(data => {
+    if (!data) {
+      res.status(404).json({message: 'invalid category.'});
+      return;
+    } else {
+      res.json(data)
+    }
+  })
+  .catch((err) => console.error('Error: ', err));
 });
 
 router.delete('/:id', (req, res) => {
-  // delete a category by its `id` value
+  Category.destroy({where: {id: req.params.id}})
+  .then(data => {
+    if (!data) {
+      res.status(404).json({message: 'invalid category.'});
+      return;
+    } else {
+      res.json(data)
+    }
+  })
 });
 
 module.exports = router;
